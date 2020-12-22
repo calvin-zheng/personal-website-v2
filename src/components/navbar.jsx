2011d8844d1285a29596bad49461fbfded8caae4
calzheng <calzheng@umich.edu>
2020-12-22 00:49:22 -0500

    Added Resume Link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -34,7 +34,7 @@ function Header () {
             <button onClick={() => scrollTo('#contact')} className="w-full md:w-auto block mt-4 md:inline-block md:mt-0 text-gray-500 hover:text-cyan-500 md:mr-4">
               Contact
             </button>
-            <a href="#download" className="inline-block px-4 py-2 leading-none border rounded text-gray-500 border-gray-500 hover:border-cyan-500 hover:text-cyan-500 mt-4 md:mt-0">Resume</a>
+            <a href="https://drive.google.com/file/d/1CwK7dPvdsEpaYNYRoieghHYBUE8pURI2/view?usp=sharing" className="inline-block px-4 py-2 leading-none border rounded text-gray-500 border-gray-500 hover:border-cyan-500 hover:text-cyan-500 mt-4 md:mt-0">Resume</a>
           </div>
         </div>
       </header>

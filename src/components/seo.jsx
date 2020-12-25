24fb6ad8140dfab30ec20608e0f09426c3f48607
calzheng <calzheng@umich.edu>
2020-12-24 20:35:36 -0500

    Fixed non-loading svg on chrome

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -44,35 +44,6 @@ const SEO = ({ title, description, image, article }) => {
 
         <meta name="google-site-verification" content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk" />
   </Helmet>
-    // <Helmet title={seo.title}>
-    //   <meta name="description" content={seo.description} />
-    //   <meta name="image" content={seo.image} />
-
-    //   {seo.url && <meta property="og:url" content={seo.url} />}
-
-    //   {(article ? true : null) && <meta property="og:type" content="article" />}
-
-    //   {seo.title && <meta property="og:title" content={seo.title} />}
-
-    //   {seo.description && (
-    //     <meta property="og:description" content={seo.description} />
-    //   )}
-
-    //   {seo.image && <meta property="og:image" content={seo.image} />}
-
-    //   <meta name="twitter:card" content="summary_large_image" />
-
-    //   {twitterUsername && (
-    //     <meta name="twitter:creator" content={twitterUsername} />
-    //   )}
-
-    //   {seo.title && <meta name="twitter:title" content={seo.title} />}
-
-    //   {seo.description && (
-    //     <meta name="twitter:description" content={seo.description} />
-    //   )}
-
-    //   {seo.image && <meta name="twitter:image" content={seo.image} />}
   )
 }
 
